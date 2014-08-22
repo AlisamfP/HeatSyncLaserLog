eaabfed2e847d56fd14f360a6b134446f4063cdb
AlisamfP <eatliightning@gmail.com>
2014-08-22 03:02:55 -0700

    can mark items as completed now

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -1,23 +1,15 @@
-//Responds to action by using newTitle property
-//as the title of a new todo whose isCompleted property is false. 
-
-//Then it clears its newTitle property, synchronizing it to the 
-//template and reseting the textfield.
-
-//Finally, it persists any unsaved changes on the todo.
-
 Todos.TodosController = Ember.ArrayController.extend({
   actions: {
     createTodo: function() {
       // Get the todo title set by the "New Todo" text field
-      var title = this.get('newTitle');
+      var title = this.get('newTitle'); //sets newTitle property as title of new todo
       if (!title) { return false; }
       if (!title.trim()) { return; }
 
       // Create the new Todo model
       var todo = this.store.createRecord('todo', {
         title: title,
-        isCompleted: false
+        isCompleted: false //sets isCompleted to false (because if they already completed it why did they add it to a todo list!)
       });
 
       // Clear the "New Todo" text field
