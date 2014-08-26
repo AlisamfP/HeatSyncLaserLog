8459d854ad97663c81466be1463fd7c4d1ba5dfa
AlisamfP <eatliightning@gmail.com>
2014-08-25 17:04:47 -0700

    links not working but I'm trying

--- js/router.js
+++ js/router.js
@@ -1,5 +1,7 @@
 LaserLog.Router.map(function() {
-  this.resource('material', { path: '/' });
+  this.resource('material', { path: '/'}, function(){
+    this.route('projects', { path: '/:material.title'});
+  });
 });
 
 LaserLog.MaterialRoute = Ember.Route.extend({
@@ -7,3 +9,17 @@ LaserLog.MaterialRoute = Ember.Route.extend({
     return this.store.find('material');
   }
 });
+
+LaserLog.ProjectsRoute = Ember.Route.extend({
+    model: function(){
+        return this.store.find('projects');
+    }
+})
+
+// LaserLog.CutsRoute = Ember.Route.extend({
+//     model: function(){
+//         Ember.$.getJSON('shortenedlaserlog.json').then(function(data){
+//             console.log(data);
+//         }, console.log('error'));
+//     }
+// })
