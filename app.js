5b07c9311eeaf5cd3b10bf7cdcb2a975cf81068b
alisamfp <eatliightning@gmail.com>
2014-10-14 16:45:32 -0700

    working with directives

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 
     app.controller('LogController', function(){
         this.materials = materials;
+        // this.cuts = cuts;
     });
 
     app.controller('TabController', function(){
@@ -15,22 +16,52 @@
         };
     });
 
-    app.controller('CutController', function(){
-      this.cut = {};
-      this.addNewCut = function(material){
-        this.cut.timestamp = Date.now();
-        material.cuts.push(this.cut);
-        this.cut = {};
-      };
 
+//     function findByName(array, name){
+//     _.forEach(array, function(obj){
+//         if(_.contains(obj, name)){
+//           console.log(obj.id);
+//             return obj.id;
+//         }
+//     })
+// }
+  
+    app.directive('tabControl', function(){
+      return{
+        restrict: 'E',
+        templateUrl: 'tab-control.html'
+      }
+    })
+
+    app.directive('cutData', function(){
+      return{
+        restrict: 'E',
+        templateUrl: 'cut-data.html'
+      };
     });
 
-    // app.directive('cutData', function(){
-    //   return{
-    //     restrict: 'E',
-    //     templateUrl: 'cut-data.html'
-    //   };
-    // });
+    app.directive('inputForm', function(){
+      return{
+        restrict: 'E',
+        templateUrl: 'input-form.html',
+        controller: function(){
+          this.cut = {};
+          this.addNewCut = function(material){
+            console.log(this.cut);
+            this.cut.timestamp = Date.now();
+            _.forEach(materials, function(item){
+              // console.log(item);
+              if(_.contains(item, material)){
+                console.log(this.cut);
+                materials[item.id].cuts.push(this.cut);
+              };
+            })
+            this.cut = {};
+        };
+        },
+        controllerAs: "cutCtrl"
+      };
+    })
 
 
     var materials = [
@@ -43,16 +74,15 @@
   userName: 'Alisa Palson',
   projectName: 'dickbutts',
   material: 'Acrylic',
-  thickness: 6,
-  speed: 10,
-  power: 40,
+  thickness: 3,
+  speed: 100,
+  power: 10,
   totalTime: 25,
   numPasses: 1,
-  engrave: {
-      cut: 30,
-      power: 300,
-      scanGap: 0.1,
-  },
+  engrave: true,
+  engraveSpeed: 30,
+  engravePower: 300,
+  engraveScanGap: 0.1,
   notes: 'word'
 },
 {
@@ -63,32 +93,36 @@
   material: 'Acrylic',
   thickness: 6,
   speed: 10,
-  power: 50,
+  power: 500,
   totalTime: 120,
   numPasses: '',
-  engrave: { },
+  engrave: false,
   notes: ''
 }]
         },
         {id: 1,
           name: 'Anodized Aluminum',
-          note: 'etching only'
+          note: 'etching only',
+          cuts: []
         },
         {id: 2,
           name: 'Corrugated Cardboard',
-          note: ''
+          // note: '',
+          cuts: []
         },
         {id: 3,
           name: 'Glass',
-          note: 'etching only'
+          note: 'etching only',
+          cuts: []
         },
         {id: 4,
           name: 'Leather',
-          note: 'naturally dyed only'
+          note: 'naturally dyed only',
+          cuts: []
         },
         {id: 5,
           name: 'MDF',
-          note: '',
+          // note: '',
           cuts: [
           {
             date: "10/11/2014",
@@ -107,42 +141,68 @@
         },
         {id: 6,
           name: 'Paper',
-          note: ''
+          // note: '',
+          cuts: []
         },
         {id: 7,
           name: 'Plywood',
-          note: ''
+          // note: '',
+          cuts: []
         },
         {id: 8,
           name: 'Fabric',
-          note: ''
+          // note: '',
+          cuts: []
         }
     ];
 
+var cuts = [
+  {
+  date: "8/8/2014",
+  time: "19:06:42",
+  userName: 'Alisa Palson',
+  projectName: 'dickbutts',
+  material: 'Acrylic',
+  thickness: 6,
+  speed: 10,
+  power: 40,
+  totalTime: 25,
+  numPasses: 1,
+  engrave: true,
+  engraveSpeed: 30,
+  engravePower: 300,
+  engraveScanGap: 0.1,
+  notes: 'word'
+  },
+  {
+  date: "8/7/2014",
+  time: "16:33:24",
+  userName: 'Dan Puffer',
+  projectName: 'Blanket Puzzle',
+  material: 'Acrylic',
+  thickness: 6,
+  speed: 10,
+  power: 50,
+  totalTime: 120,
+  numPasses: '',
+  engrave: false,
+  notes: ''
+  },
+  {
+  date: "10/11/2014",
+  time: "14:33:24",
+  userName: 'Penny The Cat',
+  projectName: 'Cat Things',
+  material: '',
+  thickness: 3,
+  speed: 20,
+  power: 500,
+  totalTime: 30,
+  numPasses: '',
+  engrave: { },
+  notes: ''
+  }
+];
 
 })();
 
-
-
-
-  // {
-  //   "Timestamp":"8/8/2014 19:06:42",
-  //   "Start Time":"8/8/2014 18:00:00",
-  //   "End Time":"8/8/2014 19:00:00",
-  //   "Project Title":"die",
-  //   "Your Name":"Tim M",
-  //   "Was the laser clean when you started?":"No",
-  //   "Did you clean the laser?":"Yes",
-  //   "Laser run time (in minutes) total for all parts/copies":25,
-  //   "Material":"Acrylic",
-  //   "Thickness (in mm)":6,
-  //   "Cutting Speed":10,
-  //   "Cutting Power":40,
-  //   "What type of use was this?":"",
-  //   "Engraving Scan Gap (if engrave)":"0.1",
-  //   "Number of passes":1,
-  //   "Project Notes":"",
-  //   "Machine Notes":"laser is crazy dirty,",
-  //   "Engraving Speed":300,
-  //   "Engraving Power":30
-  // },
